add7396da4330b768c8c25ed1f3421b4c718242f
Richard-WanderaM <richardwandera2023@gmail.com>
2023-09-14 15:35:06 +0300

    Lexical scoping and welcome message

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function welcome(firstName, lastName) {
+    var fullName = firstName + ' ' + lastName;
+  
+    function displayFullName() {
+      alert('Welcome ' + fullName + '!');
+    }
+  
+    displayFullName();
+  }
+  
+  // Test the function 
+  welcome('Holberton', 'School');
+  
