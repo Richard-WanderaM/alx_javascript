084144def4afe3044ea085af6bf261af244ec6b1
Richard-WanderaM <richardwandera2023@gmail.com>
2023-10-04 17:18:54 +0300

    updated cookies

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -2,14 +2,18 @@ function setCookies() {
     const firstNameInput = document.getElementById('firstname').value;
     const emailInput = document.getElementById('email').value;
   
-    document.cookie = `firstname=${firstNameInput}`;
-    document.cookie = `email=${emailInput}`;
+    // Get the current date and time
+    const now = new Date();
+  
+    // Calculate the expiration date (10 days from now)
+    const expirationDate = new Date(now.getTime() + 10 * 24 * 60 * 60 * 1000);
+  
+    // Format the expiration date as a string in UTC format
+    const expirationDateString = expirationDate.toUTCString();
+  
+    // Set the cookies with the expiration date and specific path
+    document.cookie = `firstname=${firstNameInput}; expires=${expirationDateString}; path=/`;
+    document.cookie = `email=${emailInput}; expires=${expirationDateString}; path=/`;
   }
   
-  function showCookies() {
-    const cookiesParagraph = document.createElement('p');
-    cookiesParagraph.innerHTML = `Cookies: ${document.cookie}`;
-  
-    document.body.appendChild(cookiesParagraph);
-  }
   
